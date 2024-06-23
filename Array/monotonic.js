a6f630ad33b5661be07313291c123c0629fa0fe3
Akshay <akshayramteke378@gmail.com>
2024-06-23 14:16:00 +0530

    monotonic array

--- Array/monotonic.js
+++ Array/monotonic.js
@@ -3,7 +3,7 @@ function monotonic(array){
     let first= array[0];
 
     let last= array[array.length-1];
-
+// case 1: first==last
     if(first===last){
 
         for(let i=0; i<array.length-1;i++){
@@ -11,7 +11,7 @@ function monotonic(array){
             if(array[i+1]!== array[i]) return false
         }
     }
-
+ //case 2: first<last
     else if (first<last){
 
         for(let i=0; i<array.length-1;i++){
@@ -19,7 +19,7 @@ function monotonic(array){
             if(array[i+1]<array[i]) return false;
     }
 }
-
+ // case 3: first >last 
           else  {
             for (let i= 0; i< array.length-1; i++) {
                 
